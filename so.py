bac7ab24f5c56bafdc4a65a04b632e036b69d432
IgnacioYanjari <ignacio.yanjari@mail.udp.cl>
2017-11-16 20:09:04 -0300

    Fumador terminado

--- so.py
+++ so.py
@@ -1,12 +1,31 @@
 import threading
 import datetime
 
-class mesa(object):
+class Mesa(object):
 	"""docstring for mesa"""
 	def __init__(self, ingredientes):
 		#super mesa, self.__init__()
 		self.ingredientes = {"cerillos":0,"papel":0,"tabaco":0}
 		self.ocupado = False
+
+	def agregar_papel(self):
+		if ocupado:
+			return -1
+		else
+			ingredientes["papel"]+=1
+
+	def agregar_cerillos(self):
+		if ocupado:
+			return -1
+		else
+			ingredientes["cerillos"]+=1
+
+	def agregar_tabaco(self):
+		if ocupado:
+			return -1
+		else
+			ingredientes["tabaco"]+=1
+
 
 	def sacar_cerillos(self):
 		if self.cosas["cerillos"]>0:
@@ -31,7 +50,7 @@ class mesa(object):
 
 
 
-class Fumador(Thread):
+class Fumar(Thread):
 	"""docstring for Fumador"""
 
 	def __init__(self,tipo,numId): #Inicialización
@@ -66,3 +85,43 @@ class Fumador(Thread):
 				if elem[1] == "papel":
 					mesa.sacar_papel()
 		time.sleep(random.randrange(1,6,1))
+
+class Fumador(object):
+	def __init__(self,tipo,numId2):
+		self.tipo=tipo
+		self.numId2 = numId2
+		self.creado = False
+
+	def Crear(self):
+		while(!self.creado):
+			pass
+		new = Fumar(self.tipo,tipes)
+		tipes+=1
+		new.start()
+
+	def Creado(self):
+		return self.creado
+
+print("Ingrese cuantas veces la mesa presentará objetos")
+n = int(input())
+mesa = Mesa()
+cnt = 0
+tipes = 1
+ingredientes = ["cerillos","tabaco","papel"]
+thread1 = Fumador("cerillos",1)
+thread2 = Fumador("tabaco",2)
+thread3 = Fumador("papel",3)
+while True :
+	if !mesa.ocupado:
+		cnt+=1
+		mesa.ingredientes[random.choice(ingredientes)]
+		mesa.ingredientes[random.choice(ingredientes)]
+		
+		if thread1.Creado() == False:
+			thread1.Crear()
+
+		if thread2.Creado() == False:
+			thread2.Crear()
+
+		if thread3.Creado() == False:
+			thread3.Crear()
